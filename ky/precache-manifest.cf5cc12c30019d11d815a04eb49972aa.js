305c260823b0fc8d356a2ab006d2e9467e39f375
Pavel R <pavel.rozhok00@gmail.com>
2020-03-27 11:45:57 +0200

    add

---
+++ ky/precache-manifest.cf5cc12c30019d11d815a04eb49972aa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df0e7c5d96254feb6797ce4712d3e65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14a09fa641093bf763c9",
+    "url": "/static/css/2.008c07f9.chunk.css"
+  },
+  {
+    "revision": "4b918fd4eb642899be45",
+    "url": "/static/css/main.e2ca43d7.chunk.css"
+  },
+  {
+    "revision": "14a09fa641093bf763c9",
+    "url": "/static/js/2.ebf30fc3.chunk.js"
+  },
+  {
+    "revision": "742a77b08ce54899eb41b35d69882a73",
+    "url": "/static/js/2.ebf30fc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b918fd4eb642899be45",
+    "url": "/static/js/main.36169915.chunk.js"
+  },
+  {
+    "revision": "6a0663cc6feac8684428",
+    "url": "/static/js/runtime-main.41fb8688.js"
+  },
+  {
+    "revision": "c36d18900cce78149fc09e7e74b4e6ac",
+    "url": "/static/media/Rich_Colon_banner.c36d1890.jpg"
+  },
+  {
+    "revision": "c970b9c3b19291ca6aa6bd249bd0d1dd",
+    "url": "/static/media/cake1.c970b9c3.gif"
+  },
+  {
+    "revision": "ed4976fc89d869a057fe056d8d4dedf5",
+    "url": "/static/media/сайт.ed4976fc.jpg"
+  }
+]);
