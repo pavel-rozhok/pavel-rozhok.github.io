8d8f79e19899ccd678b18feb0c97b2de2e4cfaf8
Pavel R <pavel.rozhok00@gmail.com>
2020-07-24 19:05:51 +0300

    add cleveroad test

---
+++ cleverod_test/precache-manifest.a4b792b4ff05c86808bcd0d82a12b4c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f25195cfaf17f396fae4c6e99a40ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ce82b01a1f6c0e6bb6e",
+    "url": "/static/css/main.a4d1125a.chunk.css"
+  },
+  {
+    "revision": "ab2e0af4e6be04e9a4f3",
+    "url": "/static/js/2.1e8310b3.chunk.js"
+  },
+  {
+    "revision": "4bb6e11100f3378c7f6cecbb95cf22f1",
+    "url": "/static/js/2.1e8310b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ce82b01a1f6c0e6bb6e",
+    "url": "/static/js/main.61d3e4a4.chunk.js"
+  },
+  {
+    "revision": "7940d449eab416bdbf21",
+    "url": "/static/js/runtime-main.1160fdaa.js"
+  }
+]);
